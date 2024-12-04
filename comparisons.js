75b55653cd1ce9d47151578b707c6f5b462977bb
Mottalib <programmermoto@gmail.com>
2024-12-04 18:45:32 +0600

    Comparisons

---
+++ comparisons.js
@@ -0,0 +1,26 @@
+// Basic Comparison
+
+// n==n
+// n>=n
+// n<=n
+// n!=n
+// n>n
+// n<n
+
+// Comparison with deffrent Data trypes
+
+// console.log(null > 1); false
+// console.log(null < 1); true
+// console.log(null <= 1); true
+// console.log(null >= 1); false
+// console.log(null == 1); false
+
+// console.log(undefined > 1); false
+// console.log(undefined < 1); false
+// console.log(undefined <= 1); false
+// console.log(undefined >= 1); false
+// console.log(undefined == 1); false
+// console.log(undefined != 1); true
+
+// Always use Strict check--> ===
+// Strict check always check value and Data types
