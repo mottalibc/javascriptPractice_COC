deff3c2a785fad5a0069641da12a55f14bdbf93f
Mottalib <programmermoto@gmail.com>
2024-12-04 17:29:15 +0600

    variables and Data Types

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+const str = "motoc";
+const num = 123;
+const bool = true / false;
+const und = undefined;
+const nul = null; // null is a object types
+const bigI = BigInt; // use for store big number
+const symB = Symbol; // use checking unique identity
+
+// Console all teh variable via Console.table
+console.table([str, num, bool, und, nul, bigI, symB]);
+
+// There are 7 types of data in premitive =>
+// number, string, boolean, null. undifined, bigInt, Symbol
+// There is a non-premitive types also available => Object
+// to check what is the data types use => typesof()/typesof method
