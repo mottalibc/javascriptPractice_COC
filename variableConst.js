c7334b0050f6438d506efee7e168670fa7aff74a
Mottalib <programmermoto@gmail.com>
2024-12-04 17:15:59 +0600

    Variable And Constant

---
+++ variableConst.js
@@ -0,0 +1,10 @@
+const id = 1121;
+let email = "moto@gmail.com";
+var city = "Cox's Bazar"; //var is accessable from anywhere which not a good practice
+collage = "CGC"; //not a good practice, not recommended
+
+// Let and const => Modern JS
+// let => can change value/variable
+// const => const cannot change value/value is constant
+// var => has a problem, var can acceable from scope to outer area
+// in js there are three scope available => Global Scope, Function Scope, block Scope
