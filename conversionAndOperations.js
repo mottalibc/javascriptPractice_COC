1ac82a5453ae51f3adea77b8921ccc162e4d731c
Mottalib <programmermoto@gmail.com>
2024-12-04 18:10:28 +0600

    Conversion

---
+++ conversionAndOperations.js
@@ -0,0 +1,42 @@
+// number conversion
+
+// let convNumber = Number(data);
+
+// console.log(typeof data);
+// console.log(typeof convNumber);
+// console.log(convNumber);
+
+// Number Method =>>
+// str => NaN
+// Boolean => true-1/false-0
+// object => NaN
+
+// String Conversion ---->
+// const data = "3132114gachkj";
+// const convStr = String(data);
+
+// console.log(typeof data);
+// console.log(typeof convStr);
+// console.log(convStr);
+
+// String Method =>>
+// object => [object object]
+// number => 123 (In String)
+// boolen => true/false
+
+// Boolen COnversion
+
+const data = 0;
+const boolConv = Boolean(data);
+
+console.log(typeof data);
+console.log(typeof boolConv);
+console.log(boolConv);
+
+// Boolen Method =>>
+// {} => true
+// "" => false
+// 123 => true
+// "str" => true
+// 0 => false
+// 1 => true
