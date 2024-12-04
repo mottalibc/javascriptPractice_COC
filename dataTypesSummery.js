f5357b538af1f900c68a820da113f4d64611e175
Mottalib <programmermoto@gmail.com>
2024-12-04 19:06:36 +0600

    Data types summery

---
+++ dataTypesSummery.js
@@ -0,0 +1,10 @@
+// Tow types of Data types in js ----->
+// Premitive->
+// String, Number, Boolean, null, undifined, BigInt, Symbol
+// Reference(Non-premitive)->
+// Array, object, Function
+
+// console.log(typeof null); Output=> Object
+// console.log(typeof undefined); Output=> undefined
+// console.log(typeof []); Output=> Object
+// console.log(typeof function () {}); Output=> function
